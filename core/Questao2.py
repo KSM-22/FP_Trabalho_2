266806e720e5cd489973cac7fce5425cd3496c17
Jhefferson Souza <188925448+jheffersonsouza@users.noreply.github.com>
2025-07-07 10:30:41 -0300

    Começando a funcionalidade da questão 2.

---
+++ core/Questao2.py
@@ -0,0 +1,15 @@
+"""
+2. Jogo de Pedra, Papel e Tesoura
+Descrição: Implementar o jogo clássico de Pedra, Papel e Tesoura, onde o usuário
+joga contra o computador.
+Estruturas utilizadas:
+• Tupla: Para armazenar as opções ("Pedra", "Papel", "Tesoura").
+• Laço while: Para permitir ao usuário jogar várias rodadas até decidir parar.
+• Condicionais (if, elif, else): Para determinar o vencedor de cada rodada.
+"""
+options =  ('Pedra', 'Papel', 'Tesoura')
+
+modes = """
+[1] Jogador x Jogador
+[2] Jogador x Computador
+"""
