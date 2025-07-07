a14e1370b8718b2b04f413caa66a88e942e109b2
Jhefferson Souza <188925448+jheffersonsouza@users.noreply.github.com>
2025-07-07 14:02:50 -0300

    Finalizando o sistema PvP da Questão 2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Trabalho-2---Fundamentos-de-Programa-o
+# Trabalho-2---Fundamentos-de-Programa-
+
+
+
+
+## Questão 2
+### Funcionalidades extras
+1. Escolher jogar contra alguém ou contra o computador.
+2. Apostar enquanto joga.
--- core/Questao2.py
+++ core/Questao2.py
@@ -63,8 +63,9 @@ def findWinner(players: dict[str, str]):
 
 def pve():
     while True:
+        print('Caso não queira apostar coloque 0 no valor.')
         money = float(input('Digite sua aposta R$'))
-        while money <= 0:
+        while money < 0:
             print('Digite um valor de dinheiro válido!')
             money = float(input('Digite sua aposta R$'))
 
@@ -80,12 +81,14 @@ def pve():
             print('Como os dois escolheram igual houve um empate!')
         else:
             print(f'O vencedor foi o {winner[0]}, pois {playerChoose} ganha de {aiChoose}!')
-            if winner[0] == 'Jogador':
-                print(f'Parabéns, você apostou R${money:.2f} e ganhou R${money * 2:.2f}')
-                print(
-                    f'No entanto, com a taxa de serviço do sistema e terceiros seu valor líquido é de R${money + 0.5:.2f}')
-            else:
-                print(f'Você perdeu, mas não se preocupe seu bonus de dinheiro foi aprimorado caso ganhe na proxima!')
+            if money != 0:
+                if winner[0] == 'Jogador':
+                    print(f'Parabéns, você apostou R${money:.2f} e ganhou R${money * 2:.2f}')
+                    print(
+                        f'No entanto, com a taxa de serviço do sistema e terceiros seu valor líquido é de R${money + 0.5:.2f}')
+                else:
+                    print(
+                        f'Você perdeu, mas não se preocupe seu bonus de dinheiro foi aprimorado caso ganhe na proxima!')
             print('-' * 60)
 
         proceedOption = str(input('Deseja continuar? [S/N] ')).upper()
@@ -99,7 +102,52 @@ def pve():
 
 
 def pvp():
-    print('nada')
+    while True:
+        betOption = str(input('Vocês vão querer apostar? (S/N)')).upper()
+        p1_name = str(input('Qual nome do Jogador 1? ')).strip().capitalize()
+        p1_choose = inputChoose()
+        p1_money = 0
+
+        p2_name = str(input('Qual nome do Jogador 2? ')).strip().capitalize()
+        p2_choose = inputChoose()
+        p2_money = 0
+        if betOption == "S":
+            p1_money = float(input(f'{p1_name}, digite o valor de sua aposta R$'))
+            while p1_money <= 0:
+                print('Digite um valor de dinheiro válido!')
+                p1_money = float(input('Digite sua aposta R$'))
+
+            p2_money = float(input(f'{p2_name},digite o valor de sua aposta R$'))
+            while p2_money <= 0:
+                print('Digite um valor de dinheiro válido!')
+                p2_money = float(input('Digite sua aposta R$'))
+
+        gameData = {p1_name: p1_choose, p2_name: p2_choose}
+
+        print('-' * 60)
+        print(f'{p1_name} escolheu {p1_choose} e {p2_name} escolheu {p2_choose}')
+        print(f'{p1_choose} x {p2_choose}')
+        print('-' * 60)
+        winner = findWinner(gameData)
+        if len(winner) > 1:
+            # Empate
+            print('Como os dois escolheram igual houve um empate!')
+        else:
+            print(
+                f'O vencedor foi o {winner[0]}, pois {gameData.pop(winner[0])} ganha de {list(gameData.values())[0]}!')
+            if betOption == 'S':
+                print(f'{p1_name} apostou R${p1_money:.2f} e {p2_name} apostou R${p2_money:.2f}')
+                print(f'Parabéns {winner[0]} você ganhou R${p1_money + p2_money:.2f}')
+            print('-' * 60)
+
+        proceedOption = str(input('Deseja continuar? [S/N] ')).upper()
+        if proceedOption == 'S':
+            continue
+        elif proceedOption == 'N':
+            break
+        else:
+            print('Opção inválida, encerrando sistema.')
+            break
 
 
 display_gameModes = '-' * 30 + """
